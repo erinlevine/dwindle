3753f87271e1765d222f5327bcd561ee4226a523
erinlevine <erinclevine@gmail.com>
2017-07-09 19:29:38 -0400

    Finished for the day

--- js/init.js
+++ js/init.js
@@ -11,8 +11,19 @@ $(document).ready(function() {
     $('#modal1').modal();
 });
 
-// $('.datepicker').pickadate({
-//    selectMonths: true, // Creates a dropdown to control month
-//   controlYear: 2017,
-//    selectYears: 100 // Creates a dropdown of 15 years to control year
-//  });
+// SIDEBAR:
+
+// Initialize collapse button
+ $(".button-collapse").sideNav();
+ // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+ $('.collapsible').collapsible();
+
+ $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) { /* Do Stuff* / }, // A function to be called when sideNav is opened
+      onClose: function(el) { /* Do Stuff* / }, // A function to be called when sideNav is closed
+    }
+  );
