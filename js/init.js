2527956eae3a2a58be03bd13a1269821b54f4af5
erinlevine <erinclevine@gmail.com>
2017-07-09 18:30:03 -0400

    Almost done with first modal

--- js/init.js
+++ js/init.js
@@ -10,3 +10,9 @@
 $(document).ready(function() {
     $('#modal1').modal();
 });
+
+// $('.datepicker').pickadate({
+//    selectMonths: true, // Creates a dropdown to control month
+//   controlYear: 2017,
+//    selectYears: 100 // Creates a dropdown of 15 years to control year
+//  });
